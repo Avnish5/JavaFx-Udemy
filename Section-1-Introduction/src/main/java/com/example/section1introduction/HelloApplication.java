d63464c94f50e28478dac057aaafa434c8ddf2e7
Avnish Kumar <avnein4988@gmail.com>
2024-11-09 12:13:07 +0530

    learning about the lifecycle of javafx application

--- Section-1-Introduction/src/main/java/com/example/section1introduction/HelloApplication.java
+++ Section-1-Introduction/src/main/java/com/example/section1introduction/HelloApplication.java
@@ -1,21 +1,37 @@
 package com.example.section1introduction;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application{
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        VBox root=new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(20,20,20,20));
+        root.setSpacing(20);
+
+        Label text=new Label("");
+        Button button=new Button("Click me");
+        button.setOnAction(e->text.setText("Welcome to my app"));
+
+
+        root.getChildren().addAll(text,button);
+
+        Scene scene=new Scene(root,350,300);
         stage.setScene(scene);
+        stage.setTitle("Hello");
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
